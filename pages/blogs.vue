85fecfe1e25f4928cecf4a65b2b2075c16f4a7ed
Bzheng1211 <bzheng1211@gmail.com>
2022-03-28 13:01:14 -0400

    Pages
    Creating page & Netlify CMS

--- pages/blogs.vue
+++ pages/blogs.vue
@@ -2,11 +2,6 @@
     <div>
 
     </div>
-    <!-- // 
-    https://vuejs.org/examples/#grid 
-    https://vuejs.org/examples/#svg
-    https://vuejs.org/examples/#crud
-    -->
 </template>
 
 <script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="body">
-    
+  
   </div>
 </template>
 
@@ -11,5 +11,19 @@ export default {
 </script>
 
 <style>
-  
+  @import url('https://fonts.googleapis.com/css2?family=Quicksand&display=swap');
+  :root{
+    /* First Theme */
+    --black-grey:#141414;
+    --blue:#1659f3;
+    --cyan:#10c1be;
+    --light-blue:#0489df;
+    --white-blue:#62bcf3;
+    /* Second Theme */
+    --dark-grey:#06101e;
+    --green-cyan:#40ceab;
+    --grey-blue:#54878d;
+    --blue-grey:#4b8cc7;
+    --white:#ffffff;
+  }
 </style>
